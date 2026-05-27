72b4558f616026c5b7a1cc81e4abe3b6cd7101a4
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-27 19:16:52 +0200

    Fix: use try exept in serialise def

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -5,15 +5,18 @@ import xml.etree.ElementTree as ET
 
 def serialize_to_xml(dictionary, filename):
     """Serialize a dictionary to an XML file."""
-    root = ET.Element("data")
+    try:
+        root = ET.Element("data")
 
-    for key, value in dictionary.items():
-        child = ET.SubElement(root, key)
-        child.text = str(value)
+        for key, value in dictionary.items():
+            child = ET.SubElement(root, key)
+            child.text = str(value)
 
-    tree = ET.ElementTree(root)
-    ET.indent(tree, space="  ")
-    tree.write(filename, encoding="utf-8", xml_declaration=True)
+        tree = ET.ElementTree(root)
+        ET.indent(tree, space="  ")
+        tree.write(filename, encoding="utf-8", xml_declaration=True)
+    except Exception:
+        pass
 
 
 def deserialize_from_xml(filename):
