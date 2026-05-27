adbccdd7ed5dced3ae08ed7b59e7de5d95161001
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-27 17:52:14 +0200

    Add: serializ and deserialize pickle file with exeption

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -7,13 +7,7 @@ class CustomObject:
     """A custom class that supports pickle-based serialization."""
 
     def __init__(self, name="", age=0, is_student=True):
-        """Initialize a CustomObject instance.
-
-        Args:
-            name (str): The name of the person.
-            age (int): The age of the person.
-            is_student (bool): Whether the person is a student.
-        """
+        """Initialize a CustomObject instance."""
         self.name = name
         self.age = age
         self.is_student = is_student
@@ -25,23 +19,21 @@ class CustomObject:
         print("Is Student: {}".format(self.is_student))
 
     def serialize(self, filename):
-        """Serialize the object and save it to a file using pickle.
-
-        Args:
-            filename (str): The path of the file to write the object to.
-        """
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
+        """Serialize the object and save it to a file using pickle."""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except (OSError, pickle.PickleError):
+            return None
 
     @classmethod
     def deserialize(cls, filename):
-        """Deserialize and return a CustomObject from a pickle file.
-
-        Args:
-            filename (str): The path of the file to read the object from.
-
-        Returns:
-            CustomObject: The deserialized object, or None on failure.
-        """
-        with open(filename, "rb") as f:
-            return pickle.load(f)
+        """Deserialize and return a CustomObject from a pickle file."""
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+                if isinstance(obj, cls):
+                    return obj
+        except Exception:
+            return None
+        return None
