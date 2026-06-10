cd273904c5ab4613cba00f41b6a6680411eb5e06
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-10 11:08:58 +0200

    Add: script Displays all values in states where name matches the argument from fb

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Displays all values in states where name matches the argument"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    query = ("SELECT * FROM states "
+             "WHERE name = '{}' "
+             "ORDER BY id ASC").format(sys.argv[4])
+    cursor.execute(query)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
