61989b2c79da7a2005f4bb2773f07c7633230aad
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-10 15:59:18 +0200

    Add: Lists all cities of a given state from hbtn_0e_4_usa — SQL injection safe"

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Lists all cities of a given state from hbtn_0e_4_usa — SQL injection safe"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """, (sys.argv[4],))
+    rows = cursor.fetchall()
+    if rows:
+        print(", ".join(row[0] for row in rows))
+    cursor.close()
+    db.close()
