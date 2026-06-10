ff36a023fcb38ac4ac6dee8e73770d9429cf34c9
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-10 10:35:55 +0200

    Add first ORM with mysql

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
