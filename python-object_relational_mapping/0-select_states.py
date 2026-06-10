e32fd53b9dab30f5216e6e03b8a5ab46b27581c9
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-10 10:38:02 +0200

    fix: use localhost

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="127.0.0.1",
+        host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
