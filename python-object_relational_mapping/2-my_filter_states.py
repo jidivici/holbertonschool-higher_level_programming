afcde558677fc0c53ac877bb2dc68311f2a918ad
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-10 11:24:11 +0200

    Fix: provoke injeciton but not forget the port now!!

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,14 +7,16 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states "
-                   "WHERE name = %s"
-                   "ORDER BY id ASC", (sys.argv[4],))
+    query = ("SELECT * FROM states "
+             "WHERE name = '{}' "
+             "ORDER BY id ASC").format(sys.argv[4])
+    cursor.execute(query)
     for row in cursor.fetchall():
         print(row)
     cursor.close()
