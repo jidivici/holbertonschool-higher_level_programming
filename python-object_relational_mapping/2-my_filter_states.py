0a651960b16a8f3f04a392d9bc8564cf23424914
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-10 15:35:17 +0200

    Fix: welcome port

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,12 +7,13 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    name = sys.argv[4].replace("'", "\\'")
+    name = sys.argv[4]
     query = ("SELECT * FROM states "
              "WHERE name = '{}' "
              "ORDER BY id ASC").format(name)
