6cb921041bec06f6e2504d72a274e22865b9ead3
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-10 15:48:57 +0200

    Fix: use query + shebang +pycode

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     query = ("SELECT * FROM states "
-            "WHERE name = '{}' "
-            "ORDER BY id ASC".format(sys.argv[4]))
+             "WHERE name = '{}' "
+             "ORDER BY id ASC".format(sys.argv[4]))
     cursor.execute(query)
     for row in cursor.fetchall():
         print(row)
