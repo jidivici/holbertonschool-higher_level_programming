0afc42c8a3c6b3f2c0d6156a17043ddca310484d
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-10 11:15:28 +0200

    Fix: avoid injection sql using direct create tuple with ,

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,10 +12,9 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    query = ("SELECT * FROM states "
-             "WHERE name = '{}' "
-             "ORDER BY id ASC").format(sys.argv[4])
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states "
+                   "WHERE name = '{}' "
+                   "ORDER BY id ASC").format(sys.argv[4],)
     for row in cursor.fetchall():
         print(row)
     cursor.close()
