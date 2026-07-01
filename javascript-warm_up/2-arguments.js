ae6cdd013e8f2ecc9e5e9f1d26d0bce770c8f7e2
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-07-01 14:39:30 +0200

    Fix: without any var

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-const nb_args = process.argv.length - 2;
-
-if (nb_args === 0) {
+if (process.argv.length - 2 === 0) {
   console.log('No argument');
-} else if (nb_args === 1) {
+} else if (process.argv.length - 2 === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
